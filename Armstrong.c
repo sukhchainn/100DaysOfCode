66582c57d7d366144c3dfdd0e5583ba217b9f7b8
Brain Developer <ssaulakh2@gmail.com>
2020-07-21 10:18:26 +0530

    Day 5

--- Armstrong.c
+++ Armstrong.c
@@ -5,19 +5,22 @@
 int armstrong(int num) {
     int i=0, temp, sum=0;
     temp = num;
+    
+    // Finding the number of digits
     while(temp != 0)
     {
         i++;
         temp = temp/10;
     }
+    // 371 should return 371
     temp = num;
     while(temp != 0)
     {
         sum += pow(temp%10, i);
         temp = temp/10;
     }
-
-    return sum;
+    // Is it an Armstrong number?
+    return num == temp ? 1 : 0;
 }
 
 int main()
